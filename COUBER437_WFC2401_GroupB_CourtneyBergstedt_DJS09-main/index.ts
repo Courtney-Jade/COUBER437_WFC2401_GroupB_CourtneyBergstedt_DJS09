f50d54c89869d30c3446e6712383bc355b184c77
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-06-10 16:50:47 +0200

    array types challenge

--- COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS09-main/index.ts
+++ COUBER437_WFC2401_GroupB_CourtneyBergstedt_DJS09-main/index.ts
@@ -1,4 +1,3 @@
-
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
@@ -35,17 +34,19 @@ showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 
 const you: {
-    firstName : string;
+    firstName: string;
     lastName: string;
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 } = {
-   firtName: 'Bobby',
-   lastName: 'Brown',
-   isReturning: true,
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-console.log(you.userName)
 
 function populateUser(isReturning : boolean, userName: string ) {
     if (isReturning){
@@ -54,4 +55,4 @@ function populateUser(isReturning : boolean, userName: string ) {
     userNameDisplay.innerHTML = userName
 }
 
-populateUser(you.isReturning, you.userName)
+populateUser(you.isReturning, you.firstName)
